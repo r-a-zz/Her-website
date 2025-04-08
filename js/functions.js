c44cb52b1dd671195ca3fee06fca62f837aa5077
Raj Abhishek Singh <136931394+r-a-zz@users.noreply.github.com>
2025-04-08 23:05:16 +0530

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -158,7 +158,7 @@ async function fetchHoroscope(sign) {
 	if (!query) return;
   
 	try {
-	  const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(query)}&key=YOUR_API_KEY&type=video&maxResults=1`);
+	  const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(query)}&key=AIzaSyCRcHBpE-l-s9pMstNwFQ0PIrNNX8IOfr0&type=video&maxResults=1`);
 	  const data = await response.json();
   
 	  const videoId = data.items[0].id.videoId;
